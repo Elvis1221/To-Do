39406ca148dd57488fe8da7f34589383518a5a4c
Andrei <fujitsuelvis@gmail.com>
2020-11-11 01:10:18 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-const addMessage = document.querySelector('.message');
 const addButton = document.querySelector('.add');
 const todo = document.querySelector('.todo');
-const helpButton = document.querySelector('#help-button');
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.close');
 
 let todoList = [];
 
 if (localStorage.getItem('todo')) {
     todoList = JSON.parse(localStorage.getItem('todo'));
+
     displayMessages()
 }
 
 addButton.addEventListener('click', function () {
+    const addMessage = document.querySelector('.message');
+
     if (!addMessage.value) return;
 
     let newTodo = {
         todo: addMessage.value,
         checked: false,
-        important: false
+        important: false,
+        id: Date.now()
     };
 
     todoList.push(newTodo);
@@ -29,17 +29,27 @@ addButton.addEventListener('click', function () {
 
 function displayMessages() {
     let displayMessage = '';
-    // if (todoList === 0) todo.innerHTML = '';
+    if (todoList.length === 0) todo.innerHTML = '';
+
     todoList.forEach(function (item, i) {
         displayMessage += `
-        <li>
-        <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}/>
+        <li class="container-message">
+        <input type='checkbox' id='item_${i}' ${item.checked ? 'checked' : ''}>
         <label for='item_${i}' class="${item.important ? 'important' : ''}">${item.todo}</label>
+        <button type='button' onclick="addDeleteItem()"  class='delete-button' id='${item.id}'></button>
 </li>
 `;
         todo.innerHTML = displayMessage
     });
 }
+
+const deleteItem = (e) => {
+    todoList = todoList.filter(item => item.id !== Number(e.target.id));
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    displayMessages();
+};
+
+const addDeleteItem = () => todo.addEventListener('click', deleteItem);
 
 todo.addEventListener('change', function (event) {
     const idInput = event.target.getAttribute('id');
@@ -57,17 +67,18 @@ todo.addEventListener('change', function (event) {
 todo.addEventListener('contextmenu', function (event) {
     event.preventDefault();
     todoList.forEach(function (item, i) {
+
         if (item.todo === event.target.innerHTML) {
-            if (event.ctrlKey || event.metaKey) {
-                todoList.splice(i, 1)
-            } else {
-                item.important = !item.important;
-            }
+            item.important = !item.important;
             displayMessages();
             localStorage.setItem('todo', JSON.stringify(todoList))
         }
     })
 });
+
+const helpButton = document.querySelector('#help-button');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close')
 
 helpButton.addEventListener('click', function (event) {
     modal.classList.add('is-open');
