730af66c73927cc0492d07eb89664db10392457f
Andrei <fujitsuelvis@gmail.com>
2020-11-11 13:01:39 +0300

    commit 11/11/2020

--- src/delete-item.js
+++
@@ -1,5 +0,0 @@
-export const deleteItem = (e,todoList,displayMessages) => {
-    todoList = todoList.filter(item => item.id !== Number(e.target.id));
-    localStorage.setItem('todo', JSON.stringify(todoList));
-    displayMessages();
-};
